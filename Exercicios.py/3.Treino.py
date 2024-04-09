c67a2269c3a78b420f01fcb5ff55f4ed3884e41e
Yatemaru <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2024-04-09 17:08:34 +0000

    WW3

---
+++ Exercicios.py/3.Treino.py
@@ -0,0 +1,72 @@
+#LIMPAR TELA 
+import os
+os. system("Cls || clear")
+
+#DECLARANDO AS VARIAS 
+nome = str (input("Digite o seu nome: "))
+n1 = int (input ("Digite o seu primeiro numero: "))
+n2 = int (input("Digite o seu segundo numero: "))
+maiorNum = 0
+menorNum = 99999999999
+
+#FAZENDO AS OPERAÇÕES
+soma = n1 + n2
+produto = n1 * n2
+media = soma / 2
+
+#COMPARANDO OS VALORES
+if maiorNum < n1:
+    maiorNum = n1
+
+if maiorNum < n2:
+    maiorNum = n1
+
+if menorNum > n1:
+    menorNum = n2
+
+if menorNum > n2:
+    menorNum = n2
+
+#CASO OS NUMERO SEJAM IGUAIS
+if n1 == n2:
+    iguais = "Os numeros são iguais"
+
+#MOSTRANDOS OS RESULTADOS
+print (media)
+print (produto)
+print (f"O seu Menor numero: {n1}")
+print (f"O seu Maior numero: {n2}")
+#LIMPAR TELA 
+import os
+os. system("Cls || clear")
+
+#DECLARANDO AS VARIAS 
+nome = str (input("Digite o seu nome: "))
+n1 = int (input ("Digite o seu primeiro numero: "))
+n2 = int (input("Digite o seu segundo numero: "))
+maiorNum = 0
+menorNum = 99999999999
+
+#FAZENDO AS OPERAÇÕES
+soma = n1 + n2
+produto = n1 * n2
+media = soma / 2
+
+#COMPARANDO OS VALORES
+if maiorNum < n1:
+    maiorNum = n1
+
+if maiorNum < n2:
+    maiorNum = n1
+
+if menorNum > n1:
+    menorNum = n2
+
+if menorNum > n2:
+    menorNum = n2
+
+#MOSTRANDOS OS RESULTADOS
+print (media)
+print (produto)
+print (f"O seu Menor numero: {n1}")
+print (f"O seu Maior numero: {n2}")
