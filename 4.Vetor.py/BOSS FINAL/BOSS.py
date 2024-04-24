738e74521a480667f432c74f5dcd5998e66802a8
Yatemaru <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2024-04-24 14:07:28 +0000

    Não contar o 0

--- 4.Vetor.py/BOSS FINAL/BOSS.py
+++ 4.Vetor.py/BOSS FINAL/BOSS.py
@@ -1,89 +1,123 @@
 import os
 os.system("cls" if os.name == "nt" else "clear")
 
+#CONSERTA QUANDO PARA NO 0
+
+
 # CRIANDO UM VETOR
 vetor = []
+i: int = 0
 
-#COMPARAÇÃO 
+#COMPARAÇÃO
 maiorNum = 0
-menorNum = 99999
+menorNum = 999999
+
+
 
 # ENTRADA DOS DADOS
-for i in range(5):
-    numero = int(input("Digite o seu numero: "))
+while True:
+    i += 1
+
+    numero = int(input(f"Digite o seu { i }ª numero: "))
     vetor.append(numero)
 
 # ESPAÇO PARA AS DEF
-def QuantidadePares(vetor):
-    pares = 0
-    for num in vetor:
-        if num % 2 == 0:
-            pares += 1
-    return pares
+    def QuantidadePares(vetor):
+        pares = 0
+        for num in vetor:
+            if num % 2 == 0 and num != 0:
+                pares += 1
+        return pares
 
-def QuantidadeImpares (vetor):
-    impares = 0
-    for num in vetor:
-        if num % 2 != 0:
-            impares += 1
-    return impares 
+    def QuantidadeImpares (vetor):
+        impares = 0
+        for num in vetor:
+            if num % 2 != 0:
+                impares += 1
+        return impares 
 
-def QuantidadePositivo (vetor):
-    positivo = 0
-    for num in vetor:
-        if num > 0:
-            positivo +=1
-    return positivo
+    def QuantidadePositivo (vetor):
+        positivo = 0
+        for num in vetor:
+            if num > 0:
+                positivo +=1
+        return positivo
 
-def QuantidadeNegative (vetor):
-    negative = 0
-    for num in vetor:
-        if num < 0:
-            negative +=1
-    return negative
+    def QuantidadeNegative (vetor):
+        negative = 0
+        for num in vetor:
+            if num < 0:
+                negative +=1
+        return negative
 
 
-# RESULTADO DAS DEF
-Pares = QuantidadePares(vetor)
-Impares = QuantidadeImpares (vetor)
-Positivo = QuantidadePositivo (vetor)
-Negativo = QuantidadeNegative (vetor)
+    # RESULTADO DAS DEF
+    Pares = QuantidadePares(vetor)
+    Impares = QuantidadeImpares (vetor)
+    Positivo = QuantidadePositivo (vetor)
+    Negativo = QuantidadeNegative (vetor)
 
-#MEDIAS DOS NUMEROS
-somaPares: int = 0
-for numero in vetor: #CHAMAR OS ELEMENTOS DO VETOR 
-    if numero % 2 == 0:
-        somaPares += numero
-        mediaPares = somaPares / Pares
+    #MEDIAS DOS NUMEROS
+    somaPares: int = 0
+    for numero in vetor: #CHAMAR OS ELEMENTOS DO VETOR 
+        if numero % 2 == 0:
+            somaPares += numero
+            mediaPares = somaPares / Pares
 
-somaImpares: int = 0
-for numero in vetor:
-    if numero % 2 != 0:
-        somaImpares += numero
-        mediaImpares = somaImpares / Impares
+    somaImpares: int = 0
+    for numero in vetor:
+        if numero % 2 != 0:
+            somaImpares += numero
+            mediaImpares = somaImpares / Impares
 
-somaPositivo: int = 0
-for numero in vetor:
-    if numero > 0:
-        somaPositivo += numero
-        mediaPositive = somaPositivo / Positivo
+    somaPositivo: int = 0
+    for numero in vetor:
+        if numero > 0:
+            somaPositivo += numero
+            mediaPositive = somaPositivo / Positivo
+        
 
-#MEDIA GERAL DOS NUMEROS
-soma = sum(vetor)
-quant = len(vetor)
-media = soma / quant
+    somaNegative: int = 0
+    for numero in vetor:
+        if numero < 0:
+            somaNegative += numero
+            mediaNegative = somaNegative / Negativo
+        else:
+           mediaNegative = 'Nenhum valor negativo digitado'
+
+    #MEDIA GERAL DOS NUMEROS
+    soma = sum(vetor)
+    quant = len(vetor)
+    media = soma / quant
+
+    #MAIOR E MENOR NUMERO
+    if numero > maiorNum:
+        maiorNum = numero
+    
+    if numero < menorNum and numero != 0:
+        menorNum = numero
+
+    #STOP LOOP
+    if numero == 0:
+        break
 
 # SAÍDA DOS DADOS
+
+os. system ('cls || clear')
 for i in reversed(vetor):
-    print(f'O seu número foi: {i}')
-'''
+    print(f'O seus {i}ª número foi: {i}')
+
 print(f"Quantidade de números pares: {Pares}")
 print (f'Quantidade de números impares {Impares}')
 print (f'Quantidade de números positivos {Positivo}')
 print (f'Quantidade de números negativos {Negativo}')
 print (f'Média geral dos números: {media}')
-'''
 
-print (mediaPares)
-print (mediaImpares)
-print (mediaPositive)
+
+print (f'Média dos Pares: {mediaPares}')
+print (f'Média dos Impares: {mediaImpares}')
+print (f'Média dos Positivos: {mediaPositive}')
+print (f'Média dos Negativos: {mediaNegative}')
+
+print (f'Maior número: {maiorNum}')
+print (f'Menor número: {menorNum}')
