2c09015904874a76ac9f1d151593b8a26fbab8ed
Yatemaru <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2024-04-24 01:13:25 +0000

    78%

--- 4.Vetor.py/BOSS FINAL/BOSS.py
+++ 4.Vetor.py/BOSS FINAL/BOSS.py
@@ -1,20 +1,74 @@
-import os 
-os. system ("cls || clear")
+import os
+os.system("cls" if os.name == "nt" else "clear")
 
+# CRIANDO UM VETOR
+vetor = []
 
-numeros = []
+#COMPARAÇÃO 
+maiorNum = 0
+menorNum = 99999
 
-pares = (int)
+# ENTRADA DOS DADOS
+for i in range(5):
+    numero = int(input("Digite o seu numero: "))
+    vetor.append(numero)
 
-#ENTRADA DOS DADOS 
-for i in range (5):
-    num: int = (input ("Digite o seu numero: "))
-    numeros.append(num)
+# ESPAÇO PARA AS DEF
+def QuantidadePares(vetor):
+    pares = 0
+    for num in vetor:
+        if num % 2 == 0:
+            pares += 1
+    return pares
 
-#ESPAÇO PARA AS DEF
+def QuantidadeImpares (vetor):
+    impares = 0
+    for num in vetor:
+        if num % 2 != 0:
+            impares += 1
+    return impares 
 
+def QuantidadePositivo (vetor):
+    positivo = 0
+    for num in vetor:
+        if num > 0:
+            positivo +=1
+    return positivo
 
-#SAIADA DOS DADOS
-for i in reversed (numeros):
-    print (i)
+def QuantidadeNegative (vetor):
+    negative = 0
+    for num in vetor:
+        if num < 0:
+            negative +=1
+    return negative
 
+#MEDIAS DOS NUMEROS
+somaPares: int = 0
+
+for numero in vetor: #CHAMAR OS ELEMENTOS DO VETOR 
+    if numero % 2 == 0:
+        somaPares += numero
+
+# RESULTADO DAS DEF
+Pares = QuantidadePares(vetor)
+Impares = QuantidadeImpares (vetor)
+Positivo = QuantidadePositivo (vetor)
+Negativo = QuantidadeNegative (vetor)
+
+#MEDIA GERAL DOS NUMEROS
+soma = sum(vetor)
+quant = len(vetor)
+media = soma / quant
+
+# SAÍDA DOS DADOS
+for i in reversed(vetor):
+    print(f'O seu número foi: {i}')
+'''
+print(f"Quantidade de números pares: {Pares}")
+print (f'Quantidade de números impares {Impares}')
+print (f'Quantidade de números positivos {Positivo}')
+print (f'Quantidade de números negativos {Negativo}')
+print (f'Média geral dos números: {media}')
+'''
+
+print (somaPares)
