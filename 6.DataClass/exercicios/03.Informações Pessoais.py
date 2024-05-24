8412b684aca2e3a376c1aad018bf59977fa568f3
Yatemaru <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2024-05-24 13:05:32 +0000

    34% concluido

--- 6.DataClass/exercicios/03.Informações Pessoais.py
+++ 6.DataClass/exercicios/03.Informações Pessoais.py
@@ -3,8 +3,10 @@ from dataclasses import dataclass
 
 os.system('cls || clear')
 
+#CRIANDO LISTA PARA GUARDA OS 5 FUNCIONÁRIOS
 conjuntoDados = []
 
+#CRIANDO A CLASS
 @dataclass
 class Usuario:
     nome: str
@@ -12,6 +14,7 @@ class Usuario:
     rg: int
     cpf: int
 
+#CRIANDO FUNÇÃO PARA ENVIAR OS DADOS PARA LISTA + DANDO INFORMAÇÕES + GUARDANDO CLASS NUMA VARIAVEL(FUNCIONARIO)
 def SolicitandoDados (conjuntoDados):
     funcionario = Usuario(
         nome = input ("Digite o seu nome: "),
@@ -20,12 +23,17 @@ def SolicitandoDados (conjuntoDados):
         cpf = input ("Digite o seu CPF: ")
     )
     conjuntoDados.append(funcionario)
+    print ("Dados do Funcionário cadastrado!!!")
 '''
 TERMINAR EM CASA LEO - _ - 
 '''
-
-
+#CRIANDO FUNÇÃO PARA CRIAR UM ARQUIVO EXTERNO
 def Salvar():
     with open("Funcionarios.txt", "w", encoding="utf-8")as arquivo:
         for funcionario in conjuntoDados:
             arquivo.write(f"Nome:{funcionario.nome}")
+
+
+#CHAMADA DA FUNÇÃO
+Salvar()
+SolicitandoDados (conjuntoDados)
