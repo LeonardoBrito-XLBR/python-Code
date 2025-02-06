5713956b9e1b769cc0ce3bb5ae135b45947a9bef
LeonardoBrito-XLBR <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2025-02-06 16:26:30 -0300

    AULA DE METEDOS - AULA -2

---
+++ vor.py
@@ -0,0 +1,29 @@
+# import os
+
+# os.system("cls")
+
+# c = 0
+# number = []
+
+# for i in range (10):
+#     num = int (input("digite o numero: "))
+    
+#     number.append(num)
+    
+# print(number)
+
+# delete = int (input("Digite o numero que vc deseja eliminar: "))
+
+# for x in number:
+#     if delete == number[c]:
+#         number.remove(x)
+#     c+=1
+        
+# print(number)
+
+
+import random
+
+numeroAleatorio = random.randint(1,10)
+
+print(numeroAleatorio)
