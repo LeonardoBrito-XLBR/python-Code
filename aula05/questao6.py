a44ee9e8246e753811951436088623ebb0d18b1b
leonardobrito-xlbr <leonardobrito2707@gmail.com>
2025-02-25 14:55:12 -0300

    validacao

--- aula05/questao6.py
+++ aula05/questao6.py
@@ -1,6 +1,6 @@
 import random
 import os
-
+os.system("cls")
 
 class Canditado: 
     def __init__(self, nome, idade, partido, votos):
@@ -8,54 +8,60 @@ class Canditado:
         self.idade = idade
         self.partido = partido
         self.votos = votos
-        
-    def exibirVotos (self):
-        return (f"A quantidade do {self.nome} votos foi: {self.votos}")
     
     def __str__(self):
         return f"Nome: {self.nome} Partido: {self.partido} Votos: {self.votos}"
-        
+
+#FUNCAO CADASTRANDO CADA CANDIDATO 
 def cadastrarCandidatos():
     nome = input("Digite o seu nome sr(a): ")
     idade = int(input("Digite sua idade: "))
-    partido = (input("Digite o seu partido (5 numeros): "))
     
-    if len(partido) > 5 or len(partido) < 5:
-        print("Impossivel cadastrar esse candidato!")
-        return
-    else:
-        print("Partido reconhecido no sistema!")
+    while True:
+        partido = (input("Digite o seu partido (5 numeros): "))
         
-   
-        votos = votosAleatorios()
-        return Canditado(nome, idade, partido, votos)         
-
-
-
+        #VALIDAÇÃO DE NUMERO DO PARTIDO
+        if len(partido) >= 6 or len(partido) <= 4:
+            print("Impossivel cadastrar esse candidato!")
+        else:
+            print("Partido reconhecido no sistema!")
+            break
+            
+    #RECEBENDO A QUANTIDADE DE VOTOS ALEATORIAMENTE
+    votos = votosAleatorios()
+        
+    #ENVINADO PARA O CLASS
+    candidato = Canditado(nome, idade, partido, votos)   
+    return candidato 
 
 
 #FUNCAO PARA RECEBER OS NUMEROS DE VOTOS 
 def votosAleatorios():
-    quantVotos = random.randint(3,9)
+    quantVotos = random.randint(3,15)
     return quantVotos
- 
- 
-# INICIO DO CODIGO! 
 
-conjCanditados = []
 
-for i in range(2):
+# FUNCAO PARA VER O RESULTADO
+def resultadoVotacao(candidatos):
+    maiorVoto = candidatos[0]
+    
+    for cand in candidatos:
+        if cand > maiorVoto:
+            maiorVoto = cand
+
+    return maiorVoto
+
+#ARRAY DE CANDIDATOS
+conjCandidatos = []
+
+#ADICIIONANDO CADA OBJETO NO ARRAY 
+for i in range(3):
     pessoa = cadastrarCandidatos()
-    conjCanditados.append(pessoa)
+    conjCandidatos.append(pessoa)
     os.system("cls")
-    
-    
-for candidato in conjCanditados:
-    print(candidato) 
-    
 
-# os.system("cls")
+for c in conjCandidatos:
+    print(c)
 
-# print("EXIBINDO AS INFORMAÇÕES")
-
-# print(pessoa.exibirVotos())
+resulatdo = resultadoVotacao(conjCandidatos)
+print(resulatdo)
