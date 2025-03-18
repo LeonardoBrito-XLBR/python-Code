6581ce4e048edf62acd4692a82f0d813958e7634
LeonardoBrito-XLBR <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2025-03-18 16:31:39 -0300

    Add files via upload

---
+++ interface/aula08/indexConversao.py
@@ -0,0 +1,30 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode('dark')
+
+intarface = ctk.CTk()
+intarface.geometry('800x500')
+
+intarface.resizable(False,False)
+
+intarface.title('Sistema de Conversao de Moeadas')
+
+intarface.iconbitmap('10384161.png')
+
+
+colorText = 'Orange'
+
+
+
+ctk.CTkLabel(intarface,
+             text="CONVERSAO DE MOEADAS",
+             font=('Consolas', 30),
+             text_color=colorText,
+             ).pack()
+
+ctk.CTkLabel(intarface,
+             text='VALOR EM REAL',
+             text_color=colorText,
+             font=('Consolas', 20)).pack()
+
+intarface.mainloop()
