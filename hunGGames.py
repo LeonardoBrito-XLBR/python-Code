74f7295d74ecce8c9676e04b55b0409f84e5e808
LeonardoBrito-XLBR <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2026-01-02 12:13:14 -0300

    Add user management functionality in hunGGames.py
    Initial implementation of user management system with login and registration options.

---
+++ hunGGames.py
@@ -0,0 +1,61 @@
+# PSEUCODIGO 
+# FAZER LOGIN - DEPENDENDO SE FOR UM ESPERCIFICO VAI SER UM ADM|USER
+
+# FUNCOES DO ADM 
+# EXIBIR TODOS OS USER, REMOVER 
+
+# FUNCOES DO USER 
+# ACESSAR O SISTEMA - CADA USER.POST(TITULO - ARTIGO - AUTOR - ANO)
+
+
+# ARMAZENAR NO BANCO DE DADOS ( ARRY )
+
+
+
+
+#SUPOSTO BANCO DE DADOS - USER 
+usuarios = []
+
+class Usuario:
+    def __init__(self, userName, nome, year, email ):
+        self.userName = userName,
+        self.nome = nome,
+        self.idade = (year - 2026),
+        self.email = email
+        
+    
+
+
+
+
+
+def MenuComando():
+    print("")
+    print("[1] ENTRAR -  USUARIOS")
+    print("[2] REGISTRAR - USUARIOS")
+    print("[3] SAIR DO SISTEMA ")
+    
+    return
+
+
+def OpcaoUser(MenuOpcoes):
+    
+    While True:
+        MenuOpcoes()
+        
+        opcao = input("DIGTE SUA OPCAO ")
+        try: 
+            opcao = int(opcao)
+        
+            if opcao in :
+                
+                return opcao 
+                
+                
+            else:
+                
+                
+        
+    
+MenuComando()
+
